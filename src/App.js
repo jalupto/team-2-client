193f8132fd551e71c5cda3339912c4166b261616
unknown <arianne.domka@gmail.com>
2021-07-13 20:47:59 -0400

    Committing changes to client side login/register endpoints

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Sitebar from './home/Navbar';
+import Auth from './auth/Auth';
 
 function App() {
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'));
+    }
+  }, [])
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  }
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('');
+  }
+  
   return (
     <div className="App">
-      
+      <Sitebar clickLogout={clearToken}/>
+      <Auth updateToken={updateToken}/>
     </div>
   );
-};
+}
 
 export default App;
