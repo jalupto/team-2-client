f5dbe45008a0bf29a35ca43e62e302ec14156b9b
Jared Upton <jalupto@gmail.com>
2021-07-14 22:50:50 -0400

    added dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import Sitebar from './components/site/Navbar';
 import Auth from './components/auth/Auth';
+import {ThemeProvider} from 'styled-components';
+import { GlobalStyles } from './components/site/darkToggle/Global';
+import { lightTheme, darkTheme } from './components/site/darkToggle/Themes';
+import { useDarkMode } from './components/site/darkToggle/useDarkMode';
+import Toggle from './components/site/darkToggle/Toggler';
 
 function App() {
   const [sessionToken, setSessionToken] = useState('');
+
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
   useEffect(() => {
     if (localStorage.getItem('token')){
       setSessionToken(localStorage.getItem('token'));
     }
-  }, [])
+  }, []);
 
   const updateToken = (newToken) => {
     localStorage.setItem('token', newToken);
     setSessionToken(newToken);
     console.log(sessionToken);
-  }
+  };
 
   const clearToken = () => {
     localStorage.clear();
     setSessionToken('');
-  }
+  };
   
+  if(!mountedComponent) return <div/>
   return (
-    <div className="App">
-      <Sitebar clickLogout={clearToken}/>
-      <Auth updateToken={updateToken}/>
-    </div>
+    <ThemeProvider theme={themeMode}>
+      <>
+      <GlobalStyles/>
+        <div className="App">
+          <Toggle theme={theme} toggleTheme={themeToggler} />
+          <Sitebar clickLogout={clearToken}/>
+          <Auth updateToken={updateToken}/>
+        </div>
+      </>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/site/darkToggle/Themes.js
@@ -0,0 +1,12 @@
+export const lightTheme = {
+    body: '#9DEBDD',
+    text: '#5C70B5',
+    toggleBorder: '#541968',
+    background: '#F05AA1',
+}
+export const darkTheme = {
+    body: '#541968',
+    text: '#9DEBDD',
+    toggleBorder: '#B2B0E2',
+    background: '#5C70B5',
+}
